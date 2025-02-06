eeed5f71aa28c4e8f6d5048202ff28b4f66ea8b5
anuragkumar-git <patelanurag3971@gmail.com>
2025-02-06 18:46:17 +0530

    5-feb: First react app with vite

---
+++ mern/5-feb/learning25/src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Content = () => {
+    let name = 'First react app';
+    return (
+        <div>
+           <h1>{name}</h1> 
+        </div>
+    )
+}
---
+++ mern/5-feb/learning25/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () =>{
+    return(
+        <div>
+            <header>
+                <h1>Header file</h1>
+            </header>
+        </div>
+    )
+}
