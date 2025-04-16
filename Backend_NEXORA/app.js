09b797fc546538a594684ead6cb41d1e82c253ee
anuragkumar-git <patelanurag3971@gmail.com>
2025-04-17 00:53:36 +0530

    refactor(app.js): add logging and security.

--- Backend_NEXORA/app.js
+++ Backend_NEXORA/app.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require("express")
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const connectDB = require('./config/db')
 const passport = require('passport')
 require('./config/googleAuth')
@@ -13,12 +16,18 @@ const port = process.env.PORT || 5000
 connectDB()
 
 // Middleware
-app.use(cors())
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(passport.initialize()); // ✅ Required to use any passport strategy
+app.use(helmet())
+app.use(morgan('dev')) //API logs
+app.use(rateLimit({ windowMs: 60 * 60 * 1000, max: 100 })) // 100/hour
 
 // Routes
+const authRoutes = require('./src/routes/authRoutes')
+app.use('/api/auth', authRoutes)
+
 const userRoutes = require("./src/routes/userRoutes")
 app.use('/api/users', userRoutes)
 
@@ -30,9 +39,6 @@ app.use('/api/deals', dealRoutes)
 
 const reviewRoutes = require('./src/routes/reviewRoutes')
 app.use('/api/reviews', reviewRoutes)
-
-const authRoutes = require('./src/routes/authRoutes')
-app.use('/api/auth', authRoutes)
 
 const reportedDealRoutes = require('./src/routes/reportedDealRoutes')
 app.use('/api/report', reportedDealRoutes)
